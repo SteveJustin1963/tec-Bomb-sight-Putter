1f49cb7f52035de38f60899713f0393d00307e21
Stephen Justin <stevejustin1963@gmail.com>
2023-05-22 23:09:58 +1000

    Update bs-putt-1.c

--- bs-putt-1.c
+++ bs-putt-1.c
@@ -1,48 +1,75 @@
+#include <stdio.h>
+#include <math.h>
+
+// Constants
+#define GRAVITY 9.81 // Acceleration due to gravity in m/s^2
+#define AIR_DENSITY_SEA_LEVEL 1.225 // Air density at sea level in kg/m^3
+#define HOLE_RADIUS 0.1 // Radius of the hole in meters (adjust as needed)
+
 // Function to calculate the optimal initial velocity
 double calculateOptimalInitialVelocity(double holeDistance) {
     // TODO: Implement the function based on the actual physics
     return 0.0;
 }
 
-// Function to calculate the optimal club angle
-double calculateOptimalClubAngle(double holeDistance) {
-    // TODO: Implement the function based on the actual physics
-    return 0.0;
+// Function to calculate the optimal club angle considering player's twist
+double calculateOptimalClubAngle(double holeDistance, double playerTwist) {
+    double optimalClubAngle = 4.0; // Optimal club angle for flat green
+    double actualClubAngle = optimalClubAngle + playerTwist; // Adjust the angle based on player's twist
+    return actualClubAngle;
 }
 
 // Function to simulate the golf ball rolling
-void simulateGolfBallRolling(double clubAngle, double clubSpeed, double ballMass, double groundFriction, double airTemperature, double grassFriction, double groundLevel, double holeDistance) {
-    double velocity = calculateInitialVelocity(clubSpeed, clubAngle);
-    double airDensity = calculateAirDensity(airTemperature, groundLevel);
-    double position = 0.0;
+void simulateGolfBallRolling(double clubAngle, double clubSpeed, double ballWeight, double groundFriction, double airTemperature, double grassFriction, double groundLevel, double distanceToHole, double playerTwist) {
+    double initialVelocity = clubSpeed * cos(clubAngle * M_PI / 180.0);
+    double initialPosition = 0.0;
+    double ballPosition = initialPosition;
+    double airDensity = AIR_DENSITY_SEA_LEVEL * exp(-groundLevel / 10000.0);
     double timeStep = 0.01;
     double time = 0.0;
 
-    double optimalVelocity = calculateOptimalInitialVelocity(holeDistance);
-    double optimalClubAngle = calculateOptimalClubAngle(holeDistance);
+    double optimalVelocity = calculateOptimalInitialVelocity(distanceToHole);
+    double optimalClubAngle = calculateOptimalClubAngle(distanceToHole, playerTwist);
 
-    printf("Time\t\tPosition\n");
+    printf("Time\t\tBall Position\n");
 
-    while (position < holeDistance) {
-        double gravitationalForce = ballMass * G;
-        double frictionForce = groundFriction * ballMass * G;
-        double airResistanceForce = 0.5 * airDensity * pow(velocity, 2) * grassFriction;
-        double acceleration = (gravitationalForce - frictionForce - airResistanceForce) / ballMass;
+    while (ballPosition < distanceToHole) {
+        double gravitationalForce = ballWeight * GRAVITY;
+        double frictionForce = groundFriction * ballWeight * GRAVITY;
+        double airResistanceForce = 0.5 * airDensity * pow(initialVelocity, 2) * grassFriction;
+        double acceleration = (gravitationalForce - frictionForce - airResistanceForce) / ballWeight;
 
         // Update the velocity and direction based on the gyro
-        velocity = velocity * 0.9 + optimalVelocity * 0.1; // A simple interpolation to simulate the gyro adjusting the velocity
-        clubAngle = clubAngle * 0.9 + optimalClubAngle * 0.1; // A simple interpolation to simulate the gyro adjusting the direction
+        initialVelocity = initialVelocity * 0.9 + optimalVelocity * 0.1; 
+        clubAngle = clubAngle * 0.9 + optimalClubAngle * 0.1;
 
-        position += velocity * timeStep * cos(clubAngle * M_PI / 180.0) + 0.5 * acceleration * pow(timeStep, 2);
-        velocity += acceleration * timeStep;
+        ballPosition += initialVelocity * timeStep + 0.5 * acceleration * pow(timeStep, 2);
+        initialVelocity += acceleration * timeStep;
+        
         time += timeStep;
-
-        printf("%.2f\t\t%.2f\n", time, position);
+        
+        printf("%.2f\t\t%.2f\n", time, ballPosition);
     }
-
-    if (position <= (holeDistance + HOLE_RADIUS)) {
+    
+    if (ballPosition <= (distanceToHole + HOLE_RADIUS)) {
         printf("\nBall IN THE HOLE!\n");
     } else {
         printf("\nBall MISSED the hole!\n");
     }
+}
+
+int main() {
+    double clubAngle = 3.0; 
+    double clubSpeed = 1.5;
+    double ballWeight = 0.0459;
+    double groundFriction = 0.2;
+    double airTemperature = 25.0; 
+    double grassFriction = 0.3;
+    double groundLevel = 0.0;
+    double distanceToHole = 10.0;
+    double playerTwist = -0.5; // The player's twist in degrees
+
+    simulateGolfBallRolling(clubAngle, clubSpeed, ballWeight, groundFriction, airTemperature, grassFriction, groundLevel, distanceToHole, playerTwist);
+
+    return 0;
 }
